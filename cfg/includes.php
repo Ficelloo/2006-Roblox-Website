f94da6f76592fa882b2d9195c6b197529be0185e
Ficelloo <59775826+Ficelloo@users.noreply.github.com>
2022-06-14 16:23:50 +0200

    Update includes.php

--- cfg/includes.php
+++ cfg/includes.php
@@ -1,3 +1,7 @@
 <?php
   include "maintenance.php";
 ?>
+
+
+<!-- By Ficello.zip#9910 if you want to see the source code: https://github.com/Ficelloo/2006-Roblox-Website --> 
+<!-- would be cool if ur not removing this credit lol -->
